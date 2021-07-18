b00dccc82fdf8424ec11134f573abfaf3b55ef25
Ben Hedgepeth <benhh@gmx.com>
2021-07-17 22:08:19 -0700

    Refactored guess updating

--- main.py
+++ main.py
@@ -7,49 +7,59 @@ class Game:
 
     def __init__(self):
         self.guesses = []
-        self.game_word = choice(game_words)
-        self.guessed_string = " _ " * len(self.game_word)
+        self.word = choice(game_words)
+        self.guessed_string = " _ " * len(self.word)
 
     def get_player_guess(self):
         letter = Player.guess_attempt()
         if any(letter == guess.letter for guess in self.guesses):
-            raise Exception
-        elif letter not in self.game_word:
+            raise Exception("That letter was already guessed. Guess again.\n")
+        elif letter not in self.word:
             return Guess(letter, "miss")
         return Guess(letter, "hit")
 
     def resolve_game_status(self):
         bad_guesses = [guess for guess in self.guesses if guess.play == "miss"]
-        if len(bad_guesses) == len(self.game_word):
-            pass
-        good_guesses = [guess for guess in self.guesses if guess.play == "hit"]
+        if bad_guesses and len(bad_guesses) == len(self.word):
+            return False
+        return True
+
+    def update_guessed_word(self, guess):
+        self.guesses.append(guess)
+        good_guesses = [
+            guess for guess in self.guesses if guess.play == "hit"
+        ]
         hangman_string = ""
-        for letter in self.game_word:
+        for letter in self.word:
             if any(letter == guess.letter for guess in good_guesses):
                 hangman_string += f"{letter} "
             else:
                 hangman_string += "_ "
-        return hangman_string.strip()
+        self.guessed_string = hangman_string.strip()
+        word = self.guessed_string.replace(" ", "").replace("_", "*")
+        return word
 
 
-
-
-
+    def add_missed_guess(guess):
+        return self.guesses.append(guess)
 
 
 def main():
     while True:
         game = Game()
         while True:
-            x = game.resolve_games_status()
-            if x:
+            player_can_guess = game.resolve_game_status()
+            if not player_can_guess:
                 break
-            else:
-                self.guessed_word = x
             try:
                 player_guess = game.get_player_guess()
             except Exception as e:
                 print(e)
                 continue
             if player_guess.is_hit():
-                pass
+                guessed_word = game.update_guessed_word(player_guess)
+            if guessed_word == game.word:
+                print("You win!")
+            else:
+                misses = game.add_missed_guess(player_guess)
+                continue
--- tests.py
+++ tests.py
@@ -39,30 +39,32 @@ class TestSomeLettersGuessed(TestCase):
     def setUp(self):
         self.game = Game()
         self.game.guesses = [
-            Guess('e', "miss"), Guess('p', 'hit'), Guess('o', 'hit')
+            Guess('e', "miss"), Guess('p', 'hit')
         ]
-        self.game.game_word = "python"
+        self.game.word = "python"
+        self.guess = Guess('o', 'hit')
 
     def test_hangman_revealed_letter_matches(self):
-        updated_hangman_string = self.game.resolve_game_status()
-        self.assertEqual(updated_hangman_string, "p _ _ _ o _")
+        updated_hangman_string = self.game.update_guessed_word(self.guess)
+        self.assertEqual(updated_hangman_string, "p***o*")
 
 
 class TestAllLettersGuessed(TestCase):
-    '''Verify that the game word string shows only
-    those letters that are guessed if the game is ongoing.'''
+    '''Verify that the game word string shows all
+    letters that are guessed if the game is ongoing.'''
 
     def setUp(self):
         self.game = Game()
         self.game.guesses = [
             Guess('y', "hit"), Guess('p', 'hit'), Guess('o', 'hit'),
-            Guess('n', "hit"), Guess('h', "hit"), Guess("t", "hit")
+            Guess('n', "hit"), Guess('h', "hit")
         ]
-        self.game.game_word = "python"
+        self.game.word = "python"
+        self.guess = Guess("t", "hit")
 
     def test_hangman_revealed_letter_matches(self):
-        updated_hangman_string = self.game.resolve_game_status()
-        self.assertEqual(updated_hangman_string, "p y t h o n")
+        updated_hangman_string = self.game.update_guessed_word(self.guess)
+        self.assertEqual(updated_hangman_string, "python")
 
 
 
